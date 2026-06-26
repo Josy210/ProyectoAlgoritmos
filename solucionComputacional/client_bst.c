10b6fc2ef1b43a8970bba0c422324bd33605aa2e
victoriaalvarado <victoriaalvri@gmail.com>
2026-06-25 18:26:14 -0600

    3.1. Gestionar clientes

--- solucionComputacional/client_bst.c
+++ solucionComputacional/client_bst.c
@@ -4,39 +4,122 @@
 #include "client_bst.h"
 
 void inicializarBST(ArbolClientes *arbol) {
-    // TODO
+    arbol->raiz = NULL;
+    arbol->tamano = 0;
+}
+static NodoBST *crearNodoBST(Cliente cliente) {
+    NodoBST *nuevo = (NodoBST *)malloc(sizeof(NodoBST));
+    if (nuevo == NULL) {
+        return NULL;
+    }
+    nuevo->dato = cliente;
+    nuevo->izquierda = NULL;
+    nuevo->derecha = NULL;
+    return nuevo;
 }
 
-// Inserta cliente - retorna 1 si éxito, 0 si id duplicado
+static int insertarRecursivo(NodoBST **raiz, Cliente cliente) {
+    if (*raiz == NULL) {
+        *raiz = crearNodoBST(cliente);
+        return *raiz != NULL;
+    }
+
+    if (cliente.id == (*raiz)->dato.id) {
+        return 0;
+    }
+    if (cliente.id < (*raiz)->dato.id) {
+        return insertarRecursivo(&(*raiz)->izquierda, cliente);
+    }
+    return insertarRecursivo(&(*raiz)->derecha, cliente);
+}
+
 int insertarCliente(ArbolClientes *arbol, Cliente cliente) {
-    // TODO
-    return 0;
+    int insertado = insertarRecursivo(&arbol->raiz, cliente);
+    if (insertado) {
+        arbol->tamano++;
+    }
+    return insertado;
 }
 
-// Busca cliente por id - retorna nodo o NULL
 NodoBST* buscarCliente(ArbolClientes *arbol, int id) {
-    // TODO
+    NodoBST *actual = arbol->raiz;
+    while (actual != NULL) {
+        if (id == actual->dato.id) {
+            return actual;
+        }
+        actual = id < actual->dato.id ? actual->izquierda : actual->derecha;
+    }
     return NULL;
 }
 
-// Retorna 1 si el cliente existe, 0 si no
 int clienteExiste(ArbolClientes *arbol, int id) {
-    // TODO
-    return 0;
+    return buscarCliente(arbol, id) != NULL;
+}
+static void imprimirCliente(Cliente cliente) {
+    printf("ID: %d | Nombre: %s | Telefono: %s | Correo: %s | Direccion: %s\n",
+           cliente.id, cliente.nombre, cliente.telefono, cliente.correo, cliente.direccion);
 }
 
+static void inOrderRec(NodoBST *nodo) {
+    if (nodo == NULL) {
+        return;
+    }
+    inOrderRec(nodo->izquierda);
+    imprimirCliente(nodo->dato);
+    inOrderRec(nodo->derecha);
+}
+static void preOrderRec(NodoBST *nodo) {
+    if (nodo == NULL) {
+        return;
+    }
+    imprimirCliente(nodo->dato);
+    preOrderRec(nodo->izquierda);
+    preOrderRec(nodo->derecha);
+}
+
+static void postOrderRec(NodoBST *nodo) {
+    if (nodo == NULL) {
+        return;
+    }
+    postOrderRec(nodo->izquierda);
+    postOrderRec(nodo->derecha);
+    imprimirCliente(nodo->dato);
+}
+
+
 void mostrarInOrder(ArbolClientes *arbol) {
-    // TODO
+    if (arbol->raiz == NULL) {
+        printf("No hay clientes registrados.\n");
+        return;
+    }
+    inOrderRec(arbol->raiz);
 }
 
 void mostrarPreOrder(ArbolClientes *arbol) {
-    // TODO
+    if (arbol->raiz == NULL) {
+        printf("No hay clientes registrados.\n");
+        return;
+    }
+    preOrderRec(arbol->raiz);
 }
 
 void mostrarPostOrder(ArbolClientes *arbol) {
-    // TODO
+    if (arbol->raiz == NULL) {
+        printf("No hay clientes registrados.\n");
+        return;
+    }
+    postOrderRec(arbol->raiz);
 }
-
+static void liberarRec(NodoBST *nodo) {
+    if (nodo == NULL) {
+        return;
+    }
+    liberarRec(nodo->izquierda);
+    liberarRec(nodo->derecha);
+    free(nodo);
+}
 void liberarBST(ArbolClientes *arbol) {
-    // TODO
+    liberarRec(arbol->raiz);
+    arbol->raiz = NULL;
+    arbol->tamano = 0;
 }
