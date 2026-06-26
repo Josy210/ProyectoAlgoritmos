361e6eef71c38d57e98b129252be345d80439a67
victoriaalvarado <victoriaalvri@gmail.com>
2026-06-25 19:09:02 -0600

    3.3. Cola de entregas pendientes :p

--- solucionComputacional/delivery_queue.c
+++ solucionComputacional/delivery_queue.c
@@ -4,38 +4,81 @@
 #include "delivery_queue.h"
 
 void inicializarCola(ColaPendientes *cola) {
-    // TODO
+    cola->frente = NULL;
+    cola->final = NULL;
+    cola->tamano = 0;
 }
 
-// Encola paquete - retorna 1 si éxito
 int enqueue(ColaPendientes *cola, Paquete paquete) {
-    // TODO
-    return 0;
-}
+    NodoCola *nuevo = (NodoCola *)malloc(sizeof(NodoCola));
+    if (nuevo == NULL) {
+        return 0;
+    }
 
-// Desencola - exito=1 si ok, exito=0 si vacía
-Paquete dequeue(ColaPendientes *cola, int *exito) {
-    Paquete vacio = {0};
-    // TODO
-    return vacio;
-}
+    strncpy(paquete.estado, estadoPendiente, maxEstado - 1);
+    paquete.estado[maxEstado - 1] = '\0';
+    nuevo->dato = paquete;
+    nuevo->siguiente = NULL;
 
-// Consulta frente sin eliminar
-Paquete peekCola(ColaPendientes *cola, int *exito) {
-    Paquete vacio = {0};
-    // TODO
-    return vacio;
-}
-
-void mostrarCola(ColaPendientes *cola) {
-    // TODO
-}
-
-int colaVacia(ColaPendientes *cola) {
-    // TODO
+    if (cola->final == NULL) {
+        cola->frente = nuevo;
+        cola->final = nuevo;
+    } else {
+        cola->final->siguiente = nuevo;
+        cola->final = nuevo;
+    }
+    cola->tamano++;
     return 1;
 }
 
+Paquete dequeue(ColaPendientes *cola, int *exito) {
+    Paquete vacio = {0};
+    if (colaVacia(cola)) {
+        *exito = 0;
+        return vacio;
+    }
+
+    NodoCola *eliminado = cola->frente;
+    Paquete paquete = eliminado->dato;
+    cola->frente = eliminado->siguiente;
+    if (cola->frente == NULL) {
+        cola->final = NULL;
+    }
+    free(eliminado);
+    cola->tamano--;
+    *exito = 1;
+    return paquete;
+}
+
+Paquete peekCola(ColaPendientes *cola, int *exito) {
+    Paquete vacio = {0};
+    if (colaVacia(cola)) {
+        *exito = 0;
+        return vacio;
+    }
+    *exito = 1;
+    return cola->frente->dato;
+}
+
+void mostrarCola(ColaPendientes *cola) {
+    if (colaVacia(cola)) {
+        printf("No hay paquetes pendientes en cola.\n");
+        return;
+    }
+
+    for (NodoCola *actual = cola->frente; actual != NULL; actual = actual->siguiente) {
+        Paquete p = actual->dato;
+        printf("Codigo: %s | Cliente: %d | Destinatario: %s | Destino: %s | Estado: %s\n",
+               p.codigo, p.idCliente, p.nombreDestinatario, p.destino, p.estado);
+    }
+}
+int colaVacia(ColaPendientes *cola) {
+    return cola->frente == NULL;
+}
+
 void liberarCola(ColaPendientes *cola) {
-    // TODO
+    int exito = 0;
+    while (!colaVacia(cola)) {
+        dequeue(cola, &exito);
+    }
 }
