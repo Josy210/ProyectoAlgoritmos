e1c20e483fc8e709f18fb36abb8126a8d152e98e
victoriaalvarado <victoriaalvri@gmail.com>
2026-06-25 19:27:14 -0600

    3.4. Pila de paquetes devueltos ;b

--- solucionComputacional/returns_stack.c
+++ solucionComputacional/returns_stack.c
@@ -4,38 +4,72 @@
 #include "returns_stack.h"
 
 void inicializarPila(PilaDevoluciones *pila) {
-    // TODO
+    pila->tope = NULL;
+    pila->tamano = 0;
 }
 
-// Apila paquete devuelto - retorna 1 si éxito
 int apilar(PilaDevoluciones *pila, Paquete paquete) {
-    // TODO
-    return 0;
-}
+    NodoPila *nuevo = (NodoPila *)malloc(sizeof(NodoPila));
+    if (nuevo == NULL) {
+        return 0;
+    }
 
-// Desapila - exito=1 si ok, exito=0 si vacía
-Paquete desapilar(PilaDevoluciones *pila, int *exito) {
-    Paquete vacio = {0};
-    // TODO
-    return vacio;
-}
-
-// Consulta tope sin eliminar
-Paquete peekPila(PilaDevoluciones *pila, int *exito) {
-    Paquete vacio = {0};
-    // TODO
-    return vacio;
-}
-
-void mostrarPila(PilaDevoluciones *pila) {
-    // TODO
-}
-
-int pilaVacia(PilaDevoluciones *pila) {
-    // TODO
+    strncpy(paquete.estado, estadoDevuelto, maxEstado - 1);
+    paquete.estado[maxEstado - 1] = '\0';
+    nuevo->dato = paquete;
+    nuevo->siguiente = pila->tope;
+    pila->tope = nuevo;
+    pila->tamano++;
     return 1;
 }
 
+Paquete desapilar(PilaDevoluciones *pila, int *exito) {
+    Paquete vacio = {0};
+    if (pilaVacia(pila)) {
+        *exito = 0;
+        return vacio;
+    }
+
+    NodoPila *eliminado = pila->tope;
+    Paquete paquete = eliminado->dato;
+    pila->tope = eliminado->siguiente;
+    free(eliminado);
+    pila->tamano--;
+    *exito = 1;
+    return paquete;
+}
+
+Paquete peekPila(PilaDevoluciones *pila, int *exito) {
+    Paquete vacio = {0};
+    if (pilaVacia(pila)) {
+        *exito = 0;
+        return vacio;
+    }
+    *exito = 1;
+    return pila->tope->dato;
+}
+
+void mostrarPila(PilaDevoluciones *pila) {
+    if (pilaVacia(pila)) {
+        printf("No hay paquetes devueltos.\n");
+        return;
+    }
+
+    for (NodoPila *actual = pila->tope; actual != NULL; actual = actual->siguiente) {
+        Paquete p = actual->dato;
+        printf("Codigo: %s | Cliente: %d | Destinatario: %s | Destino: %s | Estado: %s\n",
+               p.codigo, p.idCliente, p.nombreDestinatario, p.destino, p.estado);
+    }
+}
+
+
+int pilaVacia(PilaDevoluciones *pila) {
+    return pila->tope == NULL;
+}
+
 void liberarPila(PilaDevoluciones *pila) {
-    // TODO
+    int exito = 0;
+    while (!pilaVacia(pila)) {
+        desapilar(pila, &exito);
+    }
 }
